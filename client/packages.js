dfb910f8e528645f631699dbf161804b5ce664ce
Maxence Cornet <maxence.cornet20@gmail.com>
2016-03-30 22:27:29 +0200

    fixed route /packages

--- client/packages.js
+++
@@ -1,4 +0,0 @@
-Router.route('packages', {
-  data() { return Packages.find({}, { sort: { rank: 1 } }); },
-  waitOn() { Meteor.subscribe('packages'); },
-});
--- client/router/routes.js
+++ client/router/routes.js
@@ -4,13 +4,35 @@
 Router.route('add');
 
 Router.route('bad', {
-    data() { return Apps.find({}, { sort: { rank: 1 } }); },
-    waitOn() { Meteor.subscribe('appsAdmin', Session.get('q')); },
+    data() {
+        return Apps.find({}, {sort: {rank: 1}});
+    },
+    waitOn() {
+        Meteor.subscribe('appsAdmin', Session.get('q'));
+    },
+});
+
+Router.route('packages', {
+    data() {
+        return Packages.find({}, {sort: {rank: 1}});
+    },
+    waitOn() {
+        Meteor.subscribe('packages');
+    },
 });
 
 Router.route('/:q?', {
     name: 'home',
-    onRun() { if (this.params.q) Session.set('q', this.params.q); this.next(); },
-    data() { return Apps.find({}, { sort: { meteorRank: 1 } }); },
-    subscriptions() { return Meteor.subscribe('apps', Session.get('q')); },
+    onRun() {
+        if (this.params.q) Session.set('q', this.params.q);
+        this.next();
+    },
+    data() {
+        return Apps.find({}, {sort: {meteorRank: 1}});
+    },
+    subscriptions() {
+        return Meteor.subscribe('apps', Session.get('q'));
+    },
 });
+
+
