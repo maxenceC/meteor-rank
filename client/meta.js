757c152016ad68d2565f69de22e32e88ebd64c3d
Maxence Cornet <maxence.cornet20@gmail.com>
2016-04-05 01:04:37 +0200

    add meta tags

---
+++ client/meta.js
@@ -0,0 +1,85 @@
+/**
+ * Created by maxencecornet on 05/04/2016.
+ */
+
+Meta.config({
+    options: {
+        title: 'RankMeteor',
+        suffix: 'RankMeteor'
+    }
+});
+
+Meta.set([
+    {
+        name: 'name',
+        property: 'apple-mobile-web-app-capable',
+        content: 'yes'
+    },
+    {
+        name: 'description',
+        content: 'rankMeteor ranks meteor applications based on their alexa ranks, discover how large meteor apps are built, trends and popular meteor package'
+    },
+    /*
+     {
+     name: 'property',
+     property: 'og:image',
+     content: 'https://ayni.in/images/Logocarre.png'
+     },
+     */
+    {
+        name: 'property',
+        property: 'og:site_name',
+        content: 'RankMeteor'
+    },
+    {
+        name: 'property',
+        property: 'og:title',
+        content: 'Discover applications built with Meteor framework'
+    },
+    {
+        name: 'property',
+        property: 'og:type',
+        content: 'website'
+    },
+    {
+        name: 'property',
+        property: 'og:url',
+        content: 'http://www.rankmeteor.com/'
+    },
+    {
+        name: 'property',
+        property: 'og:description',
+        content: 'rankMeteor ranks meteor applications based on their alexa ranks, discover how large meteor apps are built, trends and popular meteor package'
+    },
+    /*
+     {
+     name: 'property',
+     property: 'fb:app_id',
+     content: '1587632601513948'
+     },
+     */
+    {
+        name: 'twitter:card',
+        content: 'summary'
+    },
+    {
+        name: 'twitter:url',
+        content: 'http://www.rankmeteor.com/'
+    },
+    {
+        name: 'twitter:title',
+        content: 'RankMeteor'
+    },
+    {
+        name: 'twitter:description',
+        content: 'rankMeteor ranks meteor applications based on their alexa ranks, discover how large meteor apps are built, trends and popular meteor package'
+    },
+    /*
+     {
+     name: 'twitter:image',
+     content: 'https://ayni.in/images/Logocarre.png'
+     }
+     */
+]);
+
+
--- client/router/routes.js
+++ client/router/routes.js
@@ -1,7 +1,15 @@
 /**
  * Created by maxencecornet on 30/03/2016.
  */
-Router.route('add');
+Router.route('add', {
+    action() {
+        Meta.setTitle('Add a meteor application');
+        Meta.set('og:title', 'Discover applications built with Meteor framework');
+        Meta.set('og:description', 'rankMeteor ranks meteor applications based on their alexa ranks, discover how large meteor apps are built, trends and popular meteor package');
+        Meta.set('og:keywords', 'meteor applications, applications built with meteor, websites built with meteor, meteor packages, packages, largest meteor app, biggest meteor website, meteor 1.3');
+        this.render();
+    }
+});
 
 Router.route('bad', {
     data() {
@@ -10,6 +18,13 @@ Router.route('bad', {
     waitOn() {
         Meteor.subscribe('appsAdmin', Session.get('q'));
     },
+    action() {
+        Meta.setTitle('Discover applications built with Meteor framework');
+        Meta.set('og:title', 'Discover applications built with Meteor framework');
+        Meta.set('og:description', 'rankMeteor ranks meteor applications based on their alexa ranks, discover how large meteor apps are built, trends and popular meteor package');
+        Meta.set('og:keywords', 'meteor applications, applications built with meteor, websites built with meteor, meteor packages, packages, largest meteor app, biggest meteor website, meteor 1.3');
+        this.render();
+    }
 });
 
 Router.route('packages', {
@@ -20,6 +35,14 @@ Router.route('packages', {
     waitOn() {
         Meteor.subscribe('packages');
     },
+    action() {
+        Meta.setTitle('Discover the most popular meteor packages');
+        Meta.set('og:title', 'Discover the most popular meteor packages');
+        Meta.set('og:description', 'rankMeteor ranks meteor applications based on their alexa ranks, discover how large meteor apps are built, trends and popular meteor package');
+        Meta.set('og:keywords', 'best meteor package, meteor applications, applications built with meteor, websites built with meteor, meteor packages, packages, largest meteor app, biggest meteor website, meteor 1.3');
+        this.render();
+
+    }
 });
 
 Router.route('/:q?', {
@@ -34,6 +57,15 @@ Router.route('/:q?', {
     subscriptions() {
         return Meteor.subscribe('apps', Session.get('q'));
     },
+    action() {
+        Meta.setTitle('Discover applications built with Meteor framework');
+        Meta.set('og:title', 'Discover applications built with Meteor framework');
+        Meta.set('og:description', 'rankMeteor ranks meteor applications based on their alexa ranks, discover how large meteor apps are built, trends and popular meteor package');
+        Meta.set('og:keywords', 'meteor applications, applications built with meteor, websites built with meteor, meteor packages, packages, largest meteor app, biggest meteor website, meteor 1.3');
+        this.render();
+    }
 });
+
+
 
 
